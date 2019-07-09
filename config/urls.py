fd53f921e770b1355f70e369aaab72f851e6b690
David Moreno <dmorenolumb@gmail.com>
2019-07-09 11:55:26 +0200

    Delete not used imports
    This commit deletes two imports not needed, this imports were
    comming from a template of a django app.
    
    Signed-off-by: David Moreno <dmorenolumb@gmail.com>

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from graphene_django.views import GraphQLView
-
-from .schema import schema
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
