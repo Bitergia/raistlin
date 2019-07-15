dc07fea5439a1c3b8ab1a2374883ec5752209d93
David Moreno <dmorenolumb@gmail.com>
2019-07-15 17:02:19 +0200

    [tasks] Get task by id
    This commit adds the functionality of getting tasks via a GET
    petition to the Django app in the url /tasks/id/<task_id>.
    
    This petition will return a JSON response with the data of the
    task if the petition went well of error if went wrong.
    
    Signed-off-by: David Moreno <dmorenolumb@gmail.com>

--- raistlin/tasks/urls.py
+++ raistlin/tasks/urls.py
@@ -6,5 +6,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='list')),
-    url(r'^list', views.TasksList.as_view(), name='tasks list')
+    url(r'^list', views.TasksList.as_view(), name='tasks list'),
+    url(r'^id/(?P<task_id>[a-zA-Z0-9_.-]+)/$', views.Task.as_view(),
+        name='task by id')
 ]
