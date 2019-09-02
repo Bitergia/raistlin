ab77d15251e0e8ba45f7538f9b356efb5c103b5b
David Moreno <dmorenolumb@gmail.com>
2019-09-02 16:58:01 +0200

    [tasks] Add view in order to add tasks to the KingArthur server
    This commit adds the url and the view class with all the logic
    needed that will parse the data sent by the ui part and then
    will send the petition to add the task to the KingArthur server.
    
    Moreover, if the response of the KingArthur server does not return
    200, a message with an error will be returned to the UI part.
    
    As we don't have users for now, it is needed to add a csrf exempt
    in order to make POST petitions, this will be eliminated when we
    add the users management.
    
    Signed-off-by: David Moreno <dmorenolumb@gmail.com>

--- raistlin/tasks/views.py
+++ raistlin/tasks/views.py
@@ -2,8 +2,10 @@ import json
 import requests
 
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 
 class TasksList(View):
@@ -43,3 +45,49 @@ class Task(View):
             return JsonResponse({'status': 'false', 'message': err},
                                 status=500)
         return JsonResponse(task, safe=False)
+
+
+class AddTask(View):
+    http_method_names = ['post']
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(AddTask, self).dispatch(*args, **kwargs)
+
+    def post(self, request):
+        task_data = json.loads(request.body)['taskData']
+        task_to_add = {
+          'tasks': [{}]
+        }
+        task_to_add['tasks'][0]['task_id'] = task_data['task_id']
+        task_to_add['tasks'][0]['backend'] = task_data['backendSelected']
+        task_to_add['tasks'][0]['backend_args'] = {}
+        if task_data['backendSelected'] == 'git':
+            task_to_add['tasks'][0]['backend_args'] = {
+                'gitpath': task_data['gitBackendArgs']['gitPath'],
+                'uri': task_data['gitBackendArgs']['uri'],
+            }
+            if 'fromDate' in task_data['gitBackendArgs']:
+                task_to_add['tasks'][0]['backend_args']['from_date'] = \
+                    task_data['gitBackendArgs']['fromDate']
+        elif task_data['backendSelected'] == 'bugzillarest':
+            task_to_add['tasks'][0]['backend_args'] = {
+                'url': task_data['bugzillarestBackendArgs']['url'],
+            }
+            if 'fromDate' in task_data['bugzillarestBackendArgs']:
+                task_to_add['tasks'][0]['backend_args']['from_date'] = \
+                    task_data['bugzillarestBackendArgs']['fromDate']
+        task_to_add['tasks'][0]['category'] = task_data['category']
+        task_to_add['tasks'][0]['scheduler'] = {
+            'delay': int(task_data['schedulerArgs']['delay']),
+            'max_retries': int(task_data['schedulerArgs']['maxRetries'])
+        }
+
+        response = requests.post(url=settings.ARTHUR_SERVER + "/add",
+                                 json=task_to_add)
+        if response.status_code != 200:
+            err = "Error adding the task to KingArthur"
+            return JsonResponse({'status': 'false', 'message': err},
+                                status=500)
+        return JsonResponse({'status': 'ok',
+                             'message': "Task added correctly"}, safe=False)
